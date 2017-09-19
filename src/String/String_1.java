3e2eab5548d6ed13aab33dd04c9bde38aa7c9c71
valor <valor@windows10.microdone.cn>
2017-09-19 17:32:17 +0800

    2017年9月19日 17:32:12
    Signed-off-by: valor <valor@windows10.microdone.cn>

--- src/String/String_1.java
+++ src/String/String_1.java
@@ -1,8 +1,14 @@
 package String;
 
+import java.text.DateFormat;
+import java.util.Date;
+
 public class String_1 {
 	public static void main(String[] args) {
 		System.out.println("1");
 		System.out.println(111111);
+		
+		
+		System.out.println("2017Äê9ÔÂ19ÈÕ 17:31:19");
 	}
 }
